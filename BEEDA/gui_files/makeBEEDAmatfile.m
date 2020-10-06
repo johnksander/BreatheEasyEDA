ff8e22524bbf9a735f51403c7d48d5282f2634bd
johnksander <john.ksander@gmail.com>
2020-10-05 21:28:26 -0400

    generic input

--- BEEDA/gui_files/makeBEEDAmatfile.m
+++ BEEDA/gui_files/makeBEEDAmatfile.m
@@ -1,77 +1,127 @@
-function BEEDAdata = makeBEEDAmatfile(BLdata,eventfile)
+function BEEDAdata = makeBEEDAmatfile(Fdata,eventfile,use_call)
 %-------------------------------------------------------------------------------------------------------
 %-------------------------------------------------------------------------------------------------------
 %-------------------------------------------------------------------------------------------------------
 %
-%makeBEEDAmatfile(BLdata,eventfile)
+%makeBEEDAmatfile(Fdata,eventfile,use_call)
 %
-%---- BLdata is the .txt data file exported from biolab
-%---- BLdata should be a .txt file with at least three columns, where the
+%---- This function converts datafiles into BEEDA mat files. The 3rd
+% argument speficies whether the input files are from biolab, or generic
+% .csv formatted files.
+%
+% FUNCTION USEAGE:
+%      makeBEEDAmatfile('subject_110_data.txt','subject_110_events.txt','biolab')
+%
+%==== If the 3rd argument specifies 'biolab'
+%
+%---- Fdata is the .txt data file exported from biolab
+%---- Fdata should be a .txt file with at least three columns, where the
 % first col is the sample time, second col is EDA response data, and the
 % third col is respiration data.
 %
 %---- eventfile is the experiment event.txt file containing event names
-% and timestamps (also biolab format) 
+% and timestamps (also biolab format)
 %
-% FUNCTION USEAGE:
-%      makeBEEDAmatfile('subject_110_data.txt','subject_110_events.txt')
+%==== If the 3rd argument specifies 'generic'
+%
+%---- Fdata should be a .csv file with at least three columns, where the
+% first col is the sample time, second col is EDA response data, and the
+% third col is respiration data.
+%
+%---- eventfile is the experiment event.csv file with two columns. First
+% column contains event names and second column contains timestamps
 %
 %-------------------------------------------------------------------------------------------------------
 %-------------------------------------------------------------------------------------------------------
 %-------------------------------------------------------------------------------------------------------
 
-disp(sprintf('reading in data file'))
-BLdata = dlmread(BLdata,'',2,0);%skip headers
-BLdata = BLdata(:,1:3); %cols (1 = timepoint) (2 = EDA) (3 = respiration)
-BLdata(:,2)= (BLdata(:,2)*10);
-%--- output SC data is off by a decimal point
 
-disp(sprintf('reading in event file'))
-%set(f_wait_text,'String','Reading in event file...')
-%drawnow 
+switch use_call
+    case 'biolab'
+        
+        disp(sprintf('reading in data file'))
+        Fdata = dlmread(Fdata,'',2,0);%skip headers
+        Fdata = Fdata(:,1:3); %cols (1 = timepoint) (2 = EDA) (3 = respiration)
+        Fdata(:,2)= (Fdata(:,2)*10);
+        %--- output SC data is off by a decimal point
+        
+        disp(sprintf('reading in event file'))
+        %set(f_wait_text,'String','Reading in event file...')
+        %drawnow
+        
+        fileID = fopen(eventfile);
+        event_array = textscan(fileID,'%s','Delimiter','\n');
+        fclose(fileID);
+        
+        disp(sprintf('reformatting and saving EDA experiment data'))
+        
+        
+        formatIn = 'dd/mm/yyyyHH:MM:SS.FFFPM';
+        
+        start_time = event_array{1}{2};
+        events = event_array{1}(3:end);
+        event_times = cell(size(events));
+        event_names = cell(size(events));
+        event_nums = cell(size(events));
+        for idx = 1:numel(event_times)
+            event_times{idx} = strsplit(events{idx});
+            hld_strs = event_times{idx}((end - 2):end);
+            event_times{idx} = [hld_strs{:}];
+            
+            event_names{idx} = strsplit(events{idx});
+            hld_strs = event_names{idx}(1:(end - 3));
+            event_names{idx} = strjoin(hld_strs);
+            
+            event_nums{idx} = strsplit(events{idx},'#');
+            event_nums{idx} = event_nums{idx}{2};
+            event_nums{idx} = strsplit(event_nums{idx},{'\t'});
+            event_nums{idx} = event_nums{idx}{1};
+        end
+        event_times = datevec(event_times,formatIn); %need datevector format
+        start_time = strsplit(start_time);
+        start_time = start_time((end - 2):end);
+        start_time = [start_time{:}];
+        start_time = datevec(start_time,formatIn); %need datevector format
+        start_time = repmat(start_time,numel(event_times(:,1)),1);
+        event_times = etime(event_times,start_time);%--- calculate difference in seconds from start time to event time
+        event_nums = str2double(event_nums);
+        
+        
+    case 'generic'
 
-fileID = fopen(eventfile);
-event_array = textscan(fileID,'%s','Delimiter','\n');
-fclose(fileID);
-
-disp(sprintf('reformatting and saving EDA experiment data'))
- 
-
-formatIn = 'dd/mm/yyyyHH:MM:SS.FFFPM';
-
-start_time = event_array{1}{2};
-events = event_array{1}(3:end);
-event_times = cell(size(events));
-event_names = cell(size(events));
-event_nums = cell(size(events));
-for idx = 1:numel(event_times)
-    event_times{idx} = strsplit(events{idx});
-    hld_strs = event_times{idx}((end - 2):end);
-    event_times{idx} = [hld_strs{:}];
-    
-    event_names{idx} = strsplit(events{idx});
-    hld_strs = event_names{idx}(1:(end - 3));
-    event_names{idx} = strjoin(hld_strs);
-    
-    event_nums{idx} = strsplit(events{idx},'#');
-    event_nums{idx} = event_nums{idx}{2};
-    event_nums{idx} = strsplit(event_nums{idx},{'\t'});
-    event_nums{idx} = event_nums{idx}{1};
+        disp(sprintf('reading in data file'))
+        Fdata = csvread(Fdata);
+        
+        disp(sprintf('reading in event file'))
+        %set(f_wait_text,'String','Reading in event file...')
+        %drawnow
+        event_data = readtable(eventfile,'Delimiter',',','ReadVariableNames',false);
+        event_data.Properties.VariableNames = {'name','timestamp'};
+        num_events = numel(event_data.name);
+        [all_events,~,event_data.number] = unique(event_data.name);
+        switch class(event_data.timestamp)
+            case 'datetime'
+                %good
+            otherwise
+                try
+                    event_data.timestamp = datetime(event_data.timestamp);
+                catch
+                    error('the timestamps in %s are not formatted properly',eventfile)
+                end
+        end
+        start_time = datetime(event_data.timestamp(1));
+         
+        event_times = etime(datevec(event_data.timestamp),datevec(start_time));
+        event_nums = event_data.number;
+        event_names = event_data.name;        
 end
-event_times = datevec(event_times,formatIn); %need datevector format
-start_time = strsplit(start_time);
-start_time = start_time((end - 2):end);
-start_time = [start_time{:}];
-start_time = datevec(start_time,formatIn); %need datevector format
-start_time = repmat(start_time,numel(event_times(:,1)),1);
-event_times = etime(event_times,start_time);%--- calculate difference in seconds from start time to event time
 
 
-BEEDAdata.respiration = BLdata(:,3)';
-BEEDAdata.EDAdata = BLdata(:,2)';
-BEEDAdata.sampletimes = BLdata(:,1)';
+BEEDAdata.respiration = Fdata(:,3)';
+BEEDAdata.EDAdata = Fdata(:,2)';
+BEEDAdata.sampletimes = Fdata(:,1)';
 
-event_nums = str2double(event_nums);
+
 
 for Eidx = 1:numel(event_times(:,1))
     BEEDAdata.event(Eidx).time = event_times(Eidx);
@@ -81,4 +131,7 @@ end
 
 BEEDAdata.savedfile = 'false';
 
+
+
 end
+
--- BEEDA/gui_files/select_intro.m
+++ BEEDA/gui_files/select_intro.m
@@ -3,32 +3,57 @@ function [FileName,PathName] = select_intro()
 FileName = [];
 PathName = [];
 
-f = figure('Visible','on','Position',[360,500,450,335],'NumberTitle','off','MenuBar', 'none','ToolBar', 'none');
+f = figure('Visible','on','Position',[360,500,450,550],'NumberTitle','off','MenuBar', 'none','ToolBar', 'none');
 f.Name = 'BreatheEasyEDA';
 
 
-
+winH = .25;
+winW = .7;
+winbuff = .075;
 Load_MWdata = uicontrol('Style','pushbutton',...
     'String','Load BioPac .txt files','Units','normalized',...
-    'Position',[.15,.65,.7,.25],'Callback',@Load_MWdata_Callback,'FontSize',25);
+    'Position',[.15,1-winH-winbuff,winW,winH],'Callback',@Load_MWdata_Callback,'FontSize',25);
 
+Load_CSVdata = uicontrol('Style','pushbutton',...
+    'String','Load generic .csv files','Units','normalized',...
+    'Position',[.15,1-winH*2-winbuff*2,winW,winH],'Callback',@Load_CSVdata_Callback,'FontSize',25);
 
 Load_BEEDAmat = uicontrol('Style','pushbutton',...
     'String','Load BEEDA .mat file','Units','normalized',...
-    'Position',[.15,.2,.7,.25],'Callback',@Load_BEEDAmat_Callback,'FontSize',25);
+    'Position',[.15,1-winH*3-winbuff*3,winW,winH],'Callback',@Load_BEEDAmat_Callback,'FontSize',25);
 
 
 
 uiwait(f)
 
     function Load_MWdata_Callback(source,eventdata)
-        [MWdata_fname,MWdata_path] = uigetfile('*.txt','Select .txt data file');
-        [eventfile_fname,eventfile_path] = uigetfile('*.txt','Select event .txt file');
+        t = 'Select data .txt file';fprintf(['\n', t,' ... \n'])
+        [MWdata_fname,MWdata_path] = uigetfile('*.txt',t);
+        t = 'Select event .txt file';fprintf(['\n', t,' ... \n\n'])
+        [eventfile_fname,eventfile_path] = uigetfile('*.txt',t);
         MWdata = fullfile(MWdata_path,MWdata_fname);
         eventfile = fullfile(eventfile_path,eventfile_fname);
         set(Load_MWdata,'String','Loading... Please wait')
         drawnow
-        BEEDAdata = makeBEEDAmatfile(MWdata,eventfile);
+        BEEDAdata = makeBEEDAmatfile(MWdata,eventfile,'biolab');
+        [sv_FileName,sv_PathName] = uiputfile('*','Save BEEDA .mat file');
+        save(fullfile(sv_PathName,sv_FileName),'BEEDAdata')
+        FileName = sv_FileName;
+        PathName = sv_PathName;
+        close(f)
+    end
+
+
+    function Load_CSVdata_Callback(source,eventdata)
+        t = 'Select data .csv file';fprintf(['\n', t,' ... \n'])
+        [CSVdata_fname,CSVdata_path] = uigetfile('*.csv',t);
+        t = 'Select event .csv file';fprintf(['\n', t,' ... \n\n'])
+        [eventfile_fname,eventfile_path] = uigetfile('*.csv',t);
+        CSVdata = fullfile(CSVdata_path,CSVdata_fname);
+        eventfile = fullfile(eventfile_path,eventfile_fname);
+        set(Load_CSVdata,'String','Loading... Please wait')
+        drawnow
+        BEEDAdata = makeBEEDAmatfile(CSVdata,eventfile,'generic');
         [sv_FileName,sv_PathName] = uiputfile('*','Save BEEDA .mat file');
         save(fullfile(sv_PathName,sv_FileName),'BEEDAdata')
         FileName = sv_FileName;
